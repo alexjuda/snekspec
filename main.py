edd1fcc13812f8e35031b0180b4aa84a7e8829a8
Alexander Juda <alexander.juda@icloud.com>
2019-07-18 22:23:29 +0200

    Add CollOf

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import typing as t
 KeysSpec = c.namedtuple('KeysSpec', ['key_specs'])
 
 
+CollOf = c.namedtuple('CollOf', ['e_spec'])
+
+
 def keys():
     pass
 
@@ -15,21 +18,47 @@ def is_valid(spec, x):
     elif isinstance(spec, KeysSpec):
         if not isinstance(x, t.Mapping):
             return False
-        if set(spec.key_specs) != set(x.keys()):
+        if spec.key_specs.keys() != x.keys():
             return False
-        return all(is_valid(key_spec, key)
-                   for key_spec, key in zip(spec.key_specs, x.keys()))
+        return all(is_valid(val_spec, val)
+                   for val_spec, val in zip(spec.key_specs.values(), x.values()))
+    elif isinstance(spec, CollOf):
+        if not isinstance(x, t.Collection):
+            return False
+        return all(is_valid(spec.e_spec, e)
+                   for e in x)
     else:
         return spec == x
 
 
+def is_string(x):
+    return isinstance(x, str)
+
+
+def is_any(x):
+    return True
+
+def is_float(x):
+    return isinstance(x, float)
+
+
 def main():
-    spec = KeysSpec(['first',
-                     'last'])
-    obj1 = {'first': 'Kamaal',
-            'last': 'Fareed'}
-    obj2 = {'first': 'Q-Tip'}
-    for i, o in enumerate([obj1, obj2]):
+    spec = KeysSpec({'first': is_any,
+                     'last': is_string,
+                     'ratings': CollOf(is_float)
+    })
+
+    objs = [{'first': 'Kamaal',
+             'last': 'Fareed',
+             'ratings': [0.6, 0.7, 0.9]},
+            {'first': 'Q-Tip'},
+            {'first': 'KRS',
+             'last': 1,
+             'ratings': [0.6, 0.7, 0.9]},
+            {'first': 'KRS',
+             'last': '1',
+             'ratings': [0.6, 0.7, 0.9]}]
+    for i, o in enumerate(objs):
         print(f'obj {i+1} valid? {is_valid(spec, o)}')
     print('ssssss')
 
