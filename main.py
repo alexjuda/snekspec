8d5ef138e3edd32fa476684bf1970c04c13c50a6
Alexander Juda <alexander.juda@icloud.com>
2019-07-18 22:29:29 +0200

    Add And

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import typing as t
 
 
 KeysSpec = c.namedtuple('KeysSpec', ['key_specs'])
-
-
 CollOf = c.namedtuple('CollOf', ['e_spec'])
+And = c.namedtuple('And', ['specs'])
 
 
 def keys():
@@ -27,6 +26,9 @@ def is_valid(spec, x):
             return False
         return all(is_valid(spec.e_spec, e)
                    for e in x)
+    elif isinstance(spec, And):
+        return all(is_valid(s, x)
+                   for s in spec.specs)
     else:
         return spec == x
 
@@ -45,19 +47,19 @@ def is_float(x):
 def main():
     spec = KeysSpec({'first': is_any,
                      'last': is_string,
-                     'ratings': CollOf(is_float)
-    })
+                     'ratings': CollOf(And([is_float,
+                                            lambda x: x > 0.6]))})
 
     objs = [{'first': 'Kamaal',
              'last': 'Fareed',
-             'ratings': [0.6, 0.7, 0.9]},
+             'ratings': [0.9, 0.7, 0.9]},
             {'first': 'Q-Tip'},
             {'first': 'KRS',
              'last': 1,
-             'ratings': [0.6, 0.7, 0.9]},
+             'ratings': [0.8, 0.7, 0.9]},
             {'first': 'KRS',
              'last': '1',
-             'ratings': [0.6, 0.7, 0.9]}]
+             'ratings': [0.5, 0.7, 0.9]}]
     for i, o in enumerate(objs):
         print(f'obj {i+1} valid? {is_valid(spec, o)}')
     print('ssssss')
