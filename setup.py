e49791f33ec75b9f4c54675a48423333bf5c7b8a
Alexander Juda <alexander.juda@icloud.com>
2019-07-21 23:45:11 +0200

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+import snekspec
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='snekspec',
+    version=snekspec.__version__,
+    author='Alexander Juda',
+    author_email='alexanderjuda@gmail.com',
+    description='Validate data & generate test examples from scheme',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/alexjuda/snekspek',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Software Development :: Testing',
+        'Intended Audience :: Developers',
+    ],
+)
