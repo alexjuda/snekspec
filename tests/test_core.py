c67123dd789652767c777b9bbb42260d7518ec6f
Alexander Juda <alexander.juda@icloud.com>
2019-07-21 13:47:42 +0200

    Reorder test asserts

--- tests/test_core.py
+++ tests/test_core.py
@@ -16,22 +16,22 @@ def test_valid_obj():
            'last': 'Fareed',
            'ratings': [0.9, 0.7, 0.9],
            'career_span': (1990, 2019)}
-    assert s.is_valid(_spec(), obj)
     assert [] == s.explain(_spec(), obj)
+    assert s.is_valid(_spec(), obj)
 
 
 def test_missing_keys():
     obj = {'first': 'Q-Tip'}
-    assert not s.is_valid(_spec(), obj)
     assert [] != s.explain(_spec(), obj)
+    assert not s.is_valid(_spec(), obj)
 
 
 def test_invalid_value():
     obj = {'first': 'KRS',
            'last': 1,
            'ratings': [0.8, 0.7, 0.9]}
-    assert not s.is_valid(_spec(), obj)
     assert [] != s.explain(_spec(), obj)
+    assert not s.is_valid(_spec(), obj)
 
 
 def test_invalid_tuple_value():
@@ -39,8 +39,8 @@ def test_invalid_tuple_value():
            'last': 'Fareed',
            'ratings': [0.9, 0.7, 0.9],
            'career_span': (1990, '*')}
-    assert not s.is_valid(_spec(), obj)
     assert [] != s.explain(_spec(), obj)
+    assert not s.is_valid(_spec(), obj)
 
 
 def test_invalid_tuple_size():
@@ -48,26 +48,26 @@ def test_invalid_tuple_size():
            'last': 'Fareed',
            'ratings': [0.9, 0.7, 0.9],
            'career_span': (1990, )}
-    assert not s.is_valid(_spec(), obj)
     assert [] != s.explain(_spec(), obj)
+    assert not s.is_valid(_spec(), obj)
 
 
 def test_invalid_nested_value():
     obj = {'first': 'KRS',
            'last': '1',
            'ratings': [0.99, 0.7, 0.8, 0.5]}
-    assert not s.is_valid(_spec(), obj)
     assert [] != s.explain(_spec(), obj)
+    assert not s.is_valid(_spec(), obj)
 
 
 def test_invalid_none():
     obj = None
-    assert not s.is_valid(_spec(), obj)
     assert [] != s.explain(_spec(), obj)
+    assert not s.is_valid(_spec(), obj)
 
 
 def test_none_with_nilable():
     obj = None
     spec = s.nilable(_spec())
-    assert s.is_valid(spec, obj)
     assert [] == s.explain(spec, obj)
+    assert s.is_valid(spec, obj)
