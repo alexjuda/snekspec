6407b1e45d3e550bbb8563e88600a024c02236e3
Alexander Juda <alexander.juda@icloud.com>
2019-07-21 23:56:09 +0200

    Add hypothesis dependency to setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/alexjuda/snekspek',
     packages=setuptools.find_packages(),
+    install_requires=['hypothesis>=4'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
